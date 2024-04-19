b01bf37fc3cf0bef3efd822d4b2029e0fc3a858c
primary pigments <primarypigments@gmail.com>
2024-04-19 08:49:31 +0000

    Configured index app and imported signals on readiness, add docstrings

--- index/apps.py
+++ index/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
 
 
+class YourAppConfig(AppConfig):
+    """
+    Application configuration for the 'index' app.
+    """
+    name = 'index'
+
+    def ready(self):
+        """
+        Imports signal handlers upon app readiness.
+        """
+        import your_app.signals
+
+
 class IndexConfig(AppConfig):
+    """
+    Configures settings for the 'index'
+    application, using BigAutoField for ID fields.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'index'
