297cf46f5779ebefeb25a9d89b2d8e0541cb5153
primary pigments <primarypigments@gmail.com>
2024-04-20 07:35:38 +0000

    Add pizzauserprofile model extending user with contact details

--- index/models.py
+++ index/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .validators import validate_customer_phone_number, validate_customer_street_address, validate_customer_city, validate_customer_zip_code
 
-# Create your models here.
+
+class PizzaUserProfile(models.Model):
+    """
+    Extends User model with contact details and address information.
+    """
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
+    phone_number = models.CharField(
+        max_length=17, validators=[validate_customer_phone_number], blank=False)
+    street_address = models.CharField(
+        max_length=100, validators=[validate_customer_street_address], blank=False)
+    city = models.CharField(
+        max_length=50, validators=[validate_customer_city], blank=True)
+    zip_code = models.CharField(
+        max_length=10, validators=[validate_customer_zip_code], blank=True)
+
+    def __str__(self):
+        """
+    Returns the username associated with the user profile.
+        """
+        return self.user.username
