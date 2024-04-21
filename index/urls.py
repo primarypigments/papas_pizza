76cf4f515c46fcbb1871807cccea5acfd9dca93b
primary pigments <primarypigments@gmail.com>
2024-04-21 11:44:22 +0000

    Complete allauth url path

--- index/urls.py
+++ index/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
     path("", views.index, name="index"),
-    path(
-        "accounts/",
-        include("django.contrib.auth.urls")
-    ),
+
+    path("accounts/", include('django.contrib.auth.urls')), 
+    
+    path("accounts/", include('allauth.urls')),
 ]
