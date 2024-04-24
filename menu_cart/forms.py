a2d572079dabca2b235c67dae044dcf4c65d7fe8
primary pigments <primarypigments@gmail.com>
2024-04-24 10:46:31 +0000

    configured meta fields to include only quantity

--- menu_cart/forms.py
+++ menu_cart/forms.py
@@ -3,3 +3,7 @@ class CartAddItemForm(forms.ModelForm):
     A form based on the CartItem model for adding items to a cart.
     Uses the 'quantity' field to specify the number of items to be added.
     """
+    class Meta:
+        model = CartItem
+        fields = ['quantity']
+
