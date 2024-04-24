a7ba53a974bfc53cb34ab2334373359c096d11fc
primary pigments <primarypigments@gmail.com>
2024-04-24 10:17:48 +0000

    Add validate_positive to enforce strictly positive numbers.

--- menu_cart/validators.py
+++ menu_cart/validators.py
@@ -10,3 +10,10 @@ def validate_non_negative(value):
         raise ValidationError(f'{value} is not a non-negative number')
 
 
+def validate_positive(value):
+    """
+    Validates that the provided value is positive (i.e., greater than 0).
+    Raises a ValidationError if the condition is not met.
+    """
+    if value <= 0:
+        raise ValidationError(f'{value} must be a positive number')
