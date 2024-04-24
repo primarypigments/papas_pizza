d4c4e5ea610639d3530d8e786515ab627d4214b7
primary pigments <primarypigments@gmail.com>
2024-04-24 09:17:32 +0000

    Formate document spacing

--- index/models.py
+++ index/models.py
@@ -5,21 +5,28 @@ from .validators import validate_customer_phone_number, validate_customer_street
 
 class PizzaUserProfile(models.Model):
     """
-    Extends User model with contact details and address information.
+    Extends User model with
+    contact details and address information.
     """
     user = models.OneToOneField(
         User, on_delete=models.CASCADE)
     phone_number = models.CharField(
-        max_length=17, validators=[validate_customer_phone_number], blank=False)
+        max_length=17, validators=[
+            validate_customer_phone_number], blank=False)
     street_address = models.CharField(
-        max_length=100, validators=[validate_customer_street_address], blank=False)
+        max_length=100, validators=[
+            validate_customer_street_address], blank=False)
     city = models.CharField(
-        max_length=50, validators=[validate_customer_city], blank=True)
+        max_length=50, validators=[
+            validate_customer_city], blank=True)
     zip_code = models.CharField(
-        max_length=10, validators=[validate_customer_zip_code], blank=True)
+        max_length=10, validators=[
+            validate_customer_zip_code], blank=True)
+
 
     def __str__(self):
         """
-    Returns the username associated with the user profile.
+    Returns the username associated
+    with the user profile.
         """
         return self.user.username
