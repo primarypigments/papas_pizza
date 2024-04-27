a435bf75acba7119510abb61421283e323f2d78e
primary pigments <primarypigments@gmail.com>
2024-04-27 11:06:08 +0000

    Create MenuItemForm with styled fields and custom labels/help texts, used for crud superuser add and edit menu items

--- menu_cart/forms.py
+++ menu_cart/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CartItem
+from .models import CartItem, MenuItem
 
 
 class CartAddItemForm(forms.ModelForm):
@@ -43,3 +43,38 @@ class UpdateCartItemForm(forms.Form):
         if quantity < 1:
             raise forms.ValidationError("Quantity must be at least 1")
         return quantity
+
+
+class MenuItemForm(forms.ModelForm):
+    """
+    A form for creating and updating MenuItem objects for the superuser.
+    It includes customized input widgets,
+    help texts, and labels to improve the user interface in form rendering.
+
+    This form is configured specifically for handling the data of
+    menu items such as pizzas,
+    including fields for the item's name, description, price, and an image.
+    """
+    class Meta:
+        """
+        Meta class for MenuItemForm. Defines the model
+        to which the form is linked and
+        specifies the model fields to include in the
+        form. Additionally, it customizes the
+        input widgets, help texts, and labels for a better user experience.
+        """
+        model = MenuItem
+        fields = ['name', 'description', 'price', 'image']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'})
+        }
+        help_texts = {
+            'name': 'Enter the name of the pizza.',
+            'description': 'Enter a brief description of the pizza.',
+        }
+        labels = {
+            'price': 'Price ($)'
+        }
