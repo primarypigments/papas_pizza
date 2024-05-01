a60d7c0a953107aa041ce8c5eecd50d1e87020fc
primary pigments <primarypigments@gmail.com>
2024-05-01 08:32:09 +0000

    Add cloudinary functionality

--- menu_cart/models.py
+++ menu_cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .validators import validate_non_negative, validate_positive
+from cloudinary.models import CloudinaryField
 
 
 class Topping(models.Model):
@@ -28,7 +29,7 @@ class MenuItem(models.Model):
     description = models.TextField()
     price = models.DecimalField(
         max_digits=6, decimal_places=2, validators=[validate_non_negative])
-    image = models.ImageField(upload_to='static/images')
+    image = CloudinaryField('image')
 
 
     def __str__(self):
