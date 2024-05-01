94c6eeea3615eefe347178283d159a0638735ee7
primary pigments <primarypigments@gmail.com>
2024-05-01 08:15:07 +0000

    Enhance pizzasignupform with placeholders and detailed field instructions

--- index/forms.py
+++ index/forms.py
@@ -13,23 +13,46 @@ class PizzaSignUpForm(UserCreationForm):
     info and address, extending UserCreationForm.
     """
     first_name = forms.CharField(
-        max_length=30, required=True, help_text='Required.')
+        max_length=30, required=True, widget=forms.TextInput(attrs={
+            'placeholder': _('First name')}), help_text='Required.')
     last_name = forms.CharField(
-        max_length=30, required=True, help_text='Required.')
+        max_length=30, required=True, widget=forms.TextInput(attrs={
+            'placeholder': _('Last name')}), help_text='Required.')
     email = forms.EmailField(
-        max_length=254, help_text='Required. Enter a valid email address.')
+        max_length=254, widget=forms.EmailInput(attrs={'placeholder': _(
+            'Email')}), help_text='Required. Enter a valid email address.')
     phone_number = forms.CharField(
         max_length=15, required=True, validators=[
-            validate_customer_phone_number], help_text='Optional.')
+            validate_customer_phone_number],
+        widget=forms.TextInput(attrs={'placeholder': _(
+            'Phone number')}), help_text='Optional.')
     street_address = forms.CharField(
         max_length=100, required=True, validators=[
-            validate_customer_street_address])
+            validate_customer_street_address],
+        widget=forms.TextInput(attrs={'placeholder': _('Street address')}))
     city = forms.CharField(
-        max_length=50, required=True, validators=[
-            validate_customer_city])
+        max_length=50, required=True, validators=[validate_customer_city],
+        widget=forms.TextInput(attrs={'placeholder': _('City')}))
     zip_code = forms.CharField(
-        max_length=10, required=True, validators=[
-            validate_customer_zip_code])
+        max_length=10, required=True, validators=[validate_customer_zip_code],
+        widget=forms.TextInput(attrs={'placeholder': _('Zip code')}))
+    newsletter_subscribe = forms.BooleanField(
+        required=False, widget=forms.CheckboxInput(), help_text=_(
+            'Check this box if you want to subscribe to our newsletter.'))
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password', 'placeholder': _('Password')}),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password', 'placeholder': _(
+                'Password confirmation')}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )    
 
     class Meta:
         """
