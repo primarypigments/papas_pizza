a39f90116b676042f7e77f16869c7644736539c3
primary pigments <primarypigments@gmail.com>
2024-05-02 12:57:25 +0000

    Add my_signup_view to urls

--- index/urls.py
+++ index/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('signup/', views.my_signup_view, name='my_signup_view'),
     
     path("accounts/", include('allauth.urls')),
 
     path('profile/', views.profile, name='profile'),
-
 ]
+
