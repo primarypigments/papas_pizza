2056fc980e8c3326c30eafa96869b0e11b05c8bc
primary pigments <primarypigments@gmail.com>
2024-05-06 07:44:01 +0000

    Format urls

--- menu_cart/urls.py
+++ menu_cart/urls.py
@@ -2,25 +2,65 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update-item/<int:item_id>/', views.update_item, name='update_item'),
-    path('remove-item/<int:item_id>/', views.remove_item, name='remove_item'),
-    path
-    ('remove-item/<int:item_id>/<str:toppings_key>/',
-    views.remove_item, name='remove_item_with_toppings'),
 
-    path('checkout/', views.checkout, name='checkout'),
+    path(
+        'update-item/<int:item_id>/',
+        views.update_item,
+        name='update_item'
+    ),
+
+    path(
+        'remove-item/<int:item_id>/',
+        views.remove_item,
+        name='remove_item'
+    ),
+
+    path(
+        'remove-item/<int:item_id>/<str:toppings_key>/',
+        views.remove_item,
+        name='remove_item_with_toppings'
+    ),
+
+    path(
+        'checkout/',
+        views.checkout,
+        name='checkout'
+    ),
+
     path(
         'checkout/success/<int:id>/',
-        views.checkout_success, name='checkout_success'),
-    path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
-    path('menu/', views.menu_view, name='menu'),
-    path('cart/', views.cart_view, name='cart'),
-    
-    path
-    ('edit_menu_item/<int:item_id>/',
-    views.edit_menu_item, name='edit_menu_item'),
+        views.checkout_success,
+        name='checkout_success'
+    ),
 
-    path
-    ('delete_menu_item/<int:item_id>/',
-    views.delete_menu_item, name='delete_menu_item'),
+    path(
+        'add-to-cart/<int:item_id>/',
+        views.add_to_cart,
+        name='add_to_cart'
+    ),
+
+    path(
+        'menu/',
+        views.menu_view,
+        name='menu'
+    ),
+
+    path(
+        'cart/',
+        views.cart_view,
+        name='cart'
+    ),
+    
+    path(
+        'edit_menu_item/<int:item_id>/',
+        views.edit_menu_item, name=
+        'edit_menu_item'
+    ),
+
+    path(
+        'delete_menu_item/<int:item_id>/',
+        views.delete_menu_item,
+        name='delete_menu_item'
+    ),
+
 ]
